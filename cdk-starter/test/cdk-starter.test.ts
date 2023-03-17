1eb50035f85fa48fa4c1b6b5ad7f767f9efcccf0
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-03-17 12:27:16 +0200

    CDK install and init

---
+++ cdk-starter/test/cdk-starter.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkStarter from '../lib/cdk-starter-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-starter-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkStarter.CdkStarterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
