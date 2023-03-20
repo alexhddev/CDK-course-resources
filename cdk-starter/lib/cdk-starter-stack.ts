69144eefb66f2f65ada6606c8ac97cf6dae6cbca
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-03-20 12:54:03 +0200

    CDK outputs

--- cdk-starter/lib/cdk-starter-stack.ts
+++ cdk-starter/lib/cdk-starter-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Duration } from 'aws-cdk-lib';
+import { CfnOutput, Duration } from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
@@ -29,11 +29,17 @@ export class CdkStarterStack extends cdk.Stack {
       }
     });
 
-    new Bucket(this, 'MyL2Bucket', {
+    const myL2Bucket = new Bucket(this, 'MyL2Bucket', {
       lifecycleRules: [{
         expiration: Duration.days(2)
       }]
     });
+    
+    new CfnOutput(this, 'MyL2BucketName', {
+      value: myL2Bucket.bucketName
+    })
+
+
 
     new L3Bucket(this, 'MyL3Bucket', 3);
 
