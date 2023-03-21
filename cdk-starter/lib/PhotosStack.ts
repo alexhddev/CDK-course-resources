596cc36a859d332a62aa539c8380b690009faaef
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-03-21 10:45:11 +0200

    CloudFormation intrinsic functions

--- cdk-starter/lib/PhotosStack.ts
+++ cdk-starter/lib/PhotosStack.ts
@@ -1,18 +1,24 @@
 import { Construct } from "constructs";
 import * as cdk from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
+import { Fn } from "aws-cdk-lib";
 
 export class PhotosStack extends cdk.Stack {
+
+    private stackSuffix: string;
+
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const myBucket = new Bucket(this, 'PhotosBucket2', {
-            bucketName: 'photosbucket-234kj35'
+        this.initializeSuffix();
+
+        new Bucket(this, 'PhotosBucket2', {
+            bucketName: `photos-bucket-${this.stackSuffix}`
         });
+    }
 
-        (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucket2234kj34')
-
-        // create a new resource
-        // delete the old one
+    private initializeSuffix(){
+        const shortStackId = Fn.select(2, Fn.split('/', this.stackId))
+        this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId))
     }
 }
