389f6ff6a04d6042dae77dd773097f3ae66de5d2
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-03-29 12:10:59 +0300

    AWS SDK

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime} from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
@@ -25,7 +26,16 @@ export class LambdaStack extends Stack {
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
             }
-        })
+        });
+
+        helloLambda.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions:[
+                's3:ListAllMyBuckets',
+                's3:ListBucket'
+            ],
+            resources: ["*"] // bad practice
+        }))
 
         this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
 
--- space-finder/src/services/hello.ts
+++ space-finder/src/services/hello.ts
@@ -1,12 +1,19 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { v4 } from 'uuid'
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3'
 
-
+const s3Client = new S3Client({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+
+    const command = new ListBucketsCommand({});
+    const listBucketsResult = (await s3Client.send(command)).Buckets;
+
+
+
     const response: APIGatewayProxyResult = {
         statusCode: 200,
-        body: JSON.stringify('Hello from lambda, this is the id:' + v4())
+        body: JSON.stringify('Hello from lambda, here are your buckets:' + JSON.stringify(listBucketsResult))
     }
     console.log(event);
 
