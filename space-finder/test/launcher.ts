ce63f6a8a468aba1a8c3fd9e9737e4035bb5cc0e
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-04 13:28:11 +0300

    Testing quries

--- space-finder/test/launcher.ts
+++ space-finder/test/launcher.ts
@@ -1,6 +1,12 @@
-import { handler } from "../src/services/hello";
+import { handler } from "../src/services/spaces/handler";
 
 
+process.env.AWS_REGION = "eu-west-1";
+process.env.TABLE_NAME = 'SpaceTable-020fda1d7783'
 
-
-handler({} as any, {} as any);
+handler({
+    httpMethod: 'POST',
+    body: JSON.stringify({
+        location: 'Dublin'
+    })
+} as any, {} as any);
