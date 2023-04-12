50ca4d4a69a8b599ebcd3a1ce8ac76d602b78793
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-12 15:24:42 +0300

    Testing the AWS temporary credentials

--- space-finder/src/infra/stacks/AuthStack.ts
+++ space-finder/src/infra/stacks/AuthStack.ts
@@ -1,7 +1,7 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib'
 import { CfnIdentityPool, CfnIdentityPoolRoleAttachment, CfnUserPoolGroup, UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { CfnUserGroup } from 'aws-cdk-lib/aws-elasticache';
-import { FederatedPrincipal, Role } from 'aws-cdk-lib/aws-iam';
+import { Effect, FederatedPrincipal, PolicyStatement, Role } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 
@@ -19,10 +19,10 @@ export class AuthStack extends Stack {
 
         this.createUserPool();
         this.createUserPoolClient();
-        this.createAdminsGroup();
         this.createIdentityPool();
         this.createRoles();
         this.attachRoles();
+        this.createAdminsGroup();
     }
 
     private createUserPool(){
@@ -55,7 +55,8 @@ export class AuthStack extends Stack {
     private createAdminsGroup(){
         new CfnUserPoolGroup(this, 'SpaceAdmins', {
             userPoolId: this.userPool.userPoolId,
-            groupName: 'admins'
+            groupName: 'admins',
+            roleArn: this.adminRole.roleArn
         })
     }
 
@@ -108,6 +109,13 @@ export class AuthStack extends Stack {
                 'sts:AssumeRoleWithWebIdentity'
             )
         });
+        this.adminRole.addToPolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                's3:ListAllMyBuckets'
+            ],
+            resources: ['*']
+        }))
     }
 
     private attachRoles(){
--- space-finder/test/auth.test.ts
+++ space-finder/test/auth.test.ts
@@ -1,3 +1,4 @@
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 import { AuthService } from "./AuthService";
 
 
@@ -9,7 +10,18 @@ async function testAuth(){
     )
     // console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
     const credentials = await service.generateTemporaryCredentials(loginResult);
-    console.log(credentials);
+    //console.log(credentials);
+    const buckets = await listBuckets(credentials);
+    console.log(buckets);
+}
+
+async function listBuckets(credentials: any){
+    const client = new S3Client({
+        credentials: credentials
+    });
+    const command = new ListBucketsCommand({});
+    const result = await client.send(command);
+    return result;
 }
 
 testAuth();
