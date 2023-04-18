5faa669699b32c391652d5319ec3c2cd186608ca
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-18 17:50:00 +0300

    AuthService with deployment

--- space-finder-frontend/src/services/AuthService.ts
+++ space-finder-frontend/src/services/AuthService.ts
@@ -1,16 +1,35 @@
+import { type CognitoUser } from '@aws-amplify/auth';
+import { Amplify, Auth } from 'aws-amplify';
+import { AuthStack } from '../../../space-finder/outputs.json';
 
+const awsRegion = 'eu-west-1';
 
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: false,
+        region: awsRegion,
+        userPoolId: AuthStack.SpaceUserPoolId,
+        userPoolWebClientId: AuthStack.SpaceUserPoolClientId,
+        identityPoolId: AuthStack.SpaceIdentityPoolId,
+        authenticationFlowType: 'USER_PASSWORD_AUTH'
+    }
+})
 
 export class AuthService {
 
+    private user: CognitoUser | undefined;
 
     public async login(userName: string, password: string):Promise<Object | undefined> {
-        return {
-            user: 'abc'
+        try {
+            this.user = await Auth.signIn(userName, password) as CognitoUser;
+            return this.user;
+        } catch (error) {
+            console.error(error);
+            return undefined
         }
     }
 
     public getUserName(){
-        return 'some user'
+        return this.user?.getUsername();
     }
 }
