ee037d7eeb89eefc9dcf760c67e69a1dd0510fc9
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-19 09:17:00 +0300

    Creating spaces

---
+++ space-finder-frontend/src/services/DataService.ts
@@ -0,0 +1,13 @@
+
+
+export class DataService {
+
+
+    public async createSpace(name: string, location:string, photo?: File){
+        return '123'
+    }
+
+    public isAuthorized(){
+        return true;
+    }
+}
