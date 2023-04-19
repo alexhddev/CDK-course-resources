5625f33c76661ec066281cb68626e2f73cefbedc
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-19 10:07:54 +0300

    Photos bucket with CORS

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -12,7 +12,9 @@ const dataStack = new DataStack(app, 'DataStack');
 const lambdaStack = new LambdaStack(app, 'LambdaStack', {
     spacesTable: dataStack.spacesTable
 });
-const authStack = new AuthStack(app, 'AuthStack');
+const authStack = new AuthStack(app, 'AuthStack', {
+    photosBucket: dataStack.photosBucket
+});
 new ApiStack(app, 'ApiStack', {
     spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
     userPool: authStack.userPool
