6f4f323ca6be2fb83c62bc233c62426804bb2271
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-19 11:44:05 +0300

    Uploading a public file

--- space-finder-frontend/src/services/DataService.ts
+++ space-finder-frontend/src/services/DataService.ts
@@ -1,9 +1,13 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { AuthService } from "./AuthService";
+import { DataStack } from '../../../space-finder/outputs.json';
 
 
 export class DataService {
 
     private authService: AuthService;
+    private s3Client: S3Client | undefined;
+    private awsRegion = 'eu-west-1';
 
     constructor(authService: AuthService) {
         this.authService = authService;
@@ -11,10 +15,30 @@ export class DataService {
 
 
     public async createSpace(name: string, location:string, photo?: File){
-        console.log('calling create space!!')
-        const credentials = await this.authService.getTemporaryCredentials();
-        console.log(credentials);
+        console.log('calling create space!!');
+        if (photo) {
+            const uploadUrl = await this.uploadPublicFile(photo);
+            console.log(uploadUrl);
+        }
         return '123'
+    }
+
+    private async uploadPublicFile(file: File){
+        const credentials = await this.authService.getTemporaryCredentials();
+        if (!this.s3Client) {
+            this.s3Client = new S3Client({
+                credentials: credentials as any,
+                region: this.awsRegion
+            });
+        }
+        const command = new PutObjectCommand({
+            Bucket: DataStack.SpaceFinderPhotosBucketName,
+            Key: file.name,
+            ACL: 'public-read',
+            Body: file
+        });
+        await this.s3Client.send(command);
+        return `https://${command.input.Bucket}.s3.${this.awsRegion}.amazonaws.com/${command.input.Key}`
     }
 
     public isAuthorized(){
