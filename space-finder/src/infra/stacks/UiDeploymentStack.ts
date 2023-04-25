dd6c05c122074719e768974c01aac85e0f4cb90f
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-25 14:00:09 +0300

    UI deployment to s3

--- space-finder/src/infra/stacks/UiDeploymentStack.ts
+++ space-finder/src/infra/stacks/UiDeploymentStack.ts
@@ -1,36 +1,52 @@
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
-import { CloudFrontWebDistribution } from "aws-cdk-lib/aws-cloudfront";
-import { IBucket } from "aws-cdk-lib/aws-s3";
-import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
-import { existsSync } from "fs";
+import { getSuffixFromStack } from "../Utils";
 import { join } from "path";
+import { existsSync } from "fs";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Distribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 
-interface UiDeploymentStackProps extends StackProps {
-    deploymentBucket: IBucket;
-}
 
 export class UiDeploymentStack extends Stack {
 
-    constructor(scope: Construct, id: string, props: UiDeploymentStackProps) {
+    constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        const suffix = getSuffixFromStack(this);
+
+        const deploymentBucket = new Bucket(this, 'uiDeploymentBucket', {
+            bucketName: `space-finder-frontend-${suffix}`
+        });
+
         const uiDir = join(__dirname, '..', '..', '..', '..', 'space-finder-frontend', 'dist');
-
-        if (existsSync(uiDir)) {
-            new BucketDeployment(this, 'space-finder-ui-deployment', {
-                destinationBucket: props.deploymentBucket,
-                sources: [
-                    Source.asset(uiDir)
-                ]
-            })
-
-            new CfnOutput(this, 'space-finder-ui-deploymentS3Url', {
-                value: props.deploymentBucket.bucketWebsiteUrl
-            })
-        } else {
-            console.warn('Ui directory not found: ' + uiDir)
+        if (!existsSync(uiDir)) {
+            console.warn('Ui dir not found: ' + uiDir);
+            return;
         }
 
+        new BucketDeployment(this, 'SpacesFinderDeployment', {
+            destinationBucket: deploymentBucket,
+            sources: [Source.asset(uiDir)]
+        });
+
+        const originIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+        deploymentBucket.grantRead(originIdentity);
+
+        const distribution = new Distribution(this, 'SpacesFinderDistribution', {
+            defaultRootObject: 'index.html',
+            defaultBehavior: {
+                origin: new S3Origin(deploymentBucket, {
+                    originAccessIdentity: originIdentity
+                })
+            }
+        });
+
+        new CfnOutput(this, 'SpaceFInderUrl', {
+            value: distribution.distributionDomainName
+        })
     }
+
+
 }
