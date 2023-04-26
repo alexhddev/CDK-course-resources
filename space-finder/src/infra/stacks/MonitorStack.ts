a8c76515a365cd019a040fa6c0b71b857d525017
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-26 14:05:47 +0300

    AWS SNS

--- space-finder/src/infra/stacks/MonitorStack.ts
+++ space-finder/src/infra/stacks/MonitorStack.ts
@@ -1,6 +1,12 @@
 import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Alarm, Metric, Unit } from "aws-cdk-lib/aws-cloudwatch";
+import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
+import { Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { LambdaSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 import { Construct } from "constructs";
+import { join } from "path";
 
 
 
@@ -8,6 +14,18 @@ export class MonitorStack extends Stack {
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const webHookLambda = new NodejsFunction(this, 'webHookLambda', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'handler',
+            entry: (join(__dirname, '..','..', 'services', 'monitor', 'handler.ts'))
+        });
+
+        const alarmTopic = new Topic(this, 'AlarmTopic', {
+            displayName: 'AlarmTopic',
+            topicName: 'AlarmTopic'
+        });
+        alarmTopic.addSubscription(new LambdaSubscription(webHookLambda));
 
         const spacesApi4xxAlarm = new Alarm(this, 'spacesApi4xxAlarm', {
             metric: new Metric({
@@ -24,5 +42,8 @@ export class MonitorStack extends Stack {
             threshold: 5,
             alarmName: 'SpacesApi4xxAlarm'
         });
+        const topicAction = new SnsAction(alarmTopic);
+        spacesApi4xxAlarm.addAlarmAction(topicAction);
+        spacesApi4xxAlarm.addOkAction(topicAction);
     }
 }
