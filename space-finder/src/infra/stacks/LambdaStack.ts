bbb832b9cfc5740602c4e900e24f98cc561abcd3
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-26 16:12:22 +0300

    AWS X-ray

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,8 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib'
+import { Duration, Stack, StackProps } from 'aws-cdk-lib'
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Runtime} from 'aws-cdk-lib/aws-lambda';
+import { Runtime, Tracing} from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
@@ -25,7 +25,9 @@ export class LambdaStack extends Stack {
             entry: (join(__dirname, '..','..', 'services', 'spaces', 'handler.ts')),
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
-            }
+            },
+            tracing: Tracing.ACTIVE,
+            timeout: Duration.minutes(1)
         });
 
         spacesLambda.addToRolePolicy(new PolicyStatement({
--- space-finder/src/services/spaces/handler.ts
+++ space-finder/src/services/spaces/handler.ts
@@ -6,13 +6,21 @@ import { updateSpace } from "./UpdateSpace";
 import { deleteSpace } from "./DeleteSpace";
 import { JsonError, MissingFieldError } from "../shared/Validator";
 import { addCorsHeader } from "../shared/Utils";
+import { captureAWSv3Client, getSegment } from 'aws-xray-sdk-core'
 
-
-const ddbClient = new DynamoDBClient({});
+const ddbClient = captureAWSv3Client(new DynamoDBClient({}))
 
 async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
     let response: APIGatewayProxyResult;
+
+    const subSeg = getSegment().addNewSubsegment('MyLongCall')
+    await new Promise(resolve =>{ setTimeout(resolve, 3000)});
+    subSeg.close();
+
+    const subSeg2 = getSegment().addNewSubsegment('MyLongCall')
+    await new Promise(resolve =>{ setTimeout(resolve, 500)})
+    subSeg2.close();
 
     try {
         switch (event.httpMethod) {
