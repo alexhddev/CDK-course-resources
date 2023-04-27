0d23941bc59d3c3e2fc636baec070e7ca3e159ad
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-27 12:02:31 +0300

    Cdk testing

--- space-finder/test/infra/MonitorStack.test.ts
+++ space-finder/test/infra/MonitorStack.test.ts
@@ -1,17 +1,79 @@
-import { App } from "aws-cdk-lib";
-import { MonitorStack } from "../../src/infra/stacks/MonitorStack";
-import { Template } from "aws-cdk-lib/assertions";
+import { App } from 'aws-cdk-lib';
+import { MonitorStack } from '../../src/infra/stacks/MonitorStack';
+import { Capture, Match, Template } from 'aws-cdk-lib/assertions';
 
 
-describe('MonitorStack test suite', ()=>{
+describe('MonitorStack test suite', () => {
 
-    test('Initial test', ()=>{
+    let monitorStackTemplate: Template;
+
+    beforeAll(() => {
         const app = new App({
             outdir: 'cdk.out.test'
         });
         const stack = new MonitorStack(app, 'MonitorStack');
+        monitorStackTemplate = Template.fromStack(stack);
+    })
 
-        const template = Template.fromStack(stack);
-        template.hasResourceProperties('AWS::Lambda::Function',{})
+    test('WebHook lambda properties', () => {
+        monitorStackTemplate.hasResourceProperties('AWS::Lambda::Function', {
+            Handler: 'index.handler',
+            Runtime: 'nodejs18.x'
+        });
+    });
+    test('Alarm topic properties', () => {
+        monitorStackTemplate.hasResourceProperties('AWS::SNS::Topic', {
+            DisplayName: 'AlarmTopic',
+            TopicName: 'AlarmTopic'
+        });
+    });
+    test('Alarm topic subscription - with matchers', () => {
+        monitorStackTemplate.hasResourceProperties('AWS::SNS::Subscription',
+            Match.objectEquals({
+                Protocol: 'lambda',
+                TopicArn: {
+                    Ref: Match.stringLikeRegexp('AlarmTopic')
+                },
+                Endpoint: {
+                    'Fn::GetAtt': [
+                        Match.stringLikeRegexp('webHookLambda'),
+                        'Arn'
+                    ]
+                }
+            }));
+    });
+
+    test('Alarm topic subscription - with exact names', () => {
+        const snsTopic = monitorStackTemplate.findResources('AWS::SNS::Topic');
+        const snsTopicName = Object.keys(snsTopic)[0];
+
+        const lambda = monitorStackTemplate.findResources('AWS::Lambda::Function');
+        const lambdaName = Object.keys(lambda)[0];
+
+        monitorStackTemplate.hasResourceProperties('AWS::SNS::Subscription',
+            {
+                Protocol: 'lambda',
+                TopicArn: {
+                    Ref: snsTopicName
+                },
+                Endpoint: {
+                    'Fn::GetAtt': [
+                        lambdaName,
+                        'Arn'
+                    ]
+                }
+            });
+    });
+
+    test('Spaces alarm with capture', () => {
+        const alarmActionsCaptor = new Capture();
+        monitorStackTemplate.hasResourceProperties('AWS::CloudWatch::Alarm', {
+            AlarmActions: alarmActionsCaptor
+        });
+
+        expect(alarmActionsCaptor.asArray()).toEqual([{
+            Ref: expect.stringMatching(/^AlarmTopic/)
+        }])
+
     })
 });
