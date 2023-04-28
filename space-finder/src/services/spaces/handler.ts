c52b4259dd51db47775abd66867fa56a438e637e
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-04-28 08:20:38 +0300

    Remove xray stuff

--- space-finder/src/services/spaces/handler.ts
+++ space-finder/src/services/spaces/handler.ts
@@ -6,21 +6,12 @@ import { updateSpace } from "./UpdateSpace";
 import { deleteSpace } from "./DeleteSpace";
 import { JsonError, MissingFieldError } from "../shared/Validator";
 import { addCorsHeader } from "../shared/Utils";
-import { captureAWSv3Client, getSegment } from 'aws-xray-sdk-core'
 
-const ddbClient = captureAWSv3Client(new DynamoDBClient({}))
+const ddbClient = new DynamoDBClient({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
     let response: APIGatewayProxyResult;
-
-    const subSeg = getSegment().addNewSubsegment('MyLongCall')
-    await new Promise(resolve =>{ setTimeout(resolve, 3000)});
-    subSeg.close();
-
-    const subSeg2 = getSegment().addNewSubsegment('MyLongCall')
-    await new Promise(resolve =>{ setTimeout(resolve, 500)})
-    subSeg2.close();
 
     try {
         switch (event.httpMethod) {
