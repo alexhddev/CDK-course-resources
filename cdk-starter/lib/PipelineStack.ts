3502d0ee68fc5ee4bf67bf5493da0ff1495c1b27
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-05-03 14:51:51 +0300

    test

--- cdk-starter/lib/PipelineStack.ts
+++ cdk-starter/lib/PipelineStack.ts
@@ -18,6 +18,7 @@ export class PipeLineStack extends Stack {
                 ],
                 primaryOutputDirectory: 'cdk-starter/cdk.out'
             })
-        })
+        });
+        
     }
 }
