7609bed3088693a85ea590ff8451c823fb644889
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-05-03 12:27:45 +0300

    Call in bin

--- cdk-starter/bin/cdk-starter.ts
+++ cdk-starter/bin/cdk-starter.ts
@@ -4,6 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { PhotosStack } from '../lib/PhotosStack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
 import { BucketTagger } from './Tagger';
+import { PipeLineStack } from '../lib/PipeLineStack';
 
 const app = new cdk.App();
 const photosStack = new PhotosStack(app, 'PhotosStack');
@@ -11,5 +12,9 @@ new PhotosHandlerStack(app, 'PhotosHandlerStack', {
     targetBucketArn: photosStack.photosBucketArn
 });
 
+new PipeLineStack(app, 'PipelineStack')
+
 const tagger = new BucketTagger('level', 'test');
 cdk.Aspects.of(app).add(tagger);
+
+app.synth();
