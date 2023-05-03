24bedd0266402105a45617f37edfe56cdf9cdf34
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-05-03 12:26:06 +0300

    Pipe test

---
+++ cdk-starter/lib/PipelineStack.ts
@@ -0,0 +1,22 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+
+
+export class PipeLineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'AwesomePipeline',
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.gitHub('alexhddev/CDK-course-resources', 'pipe-test'),
+                commands: [
+                    'cd cdk-starter',
+                    'npm ci',
+                    'npm run cdk synth'
+                ]
+            })
+        })
+    }
+}
