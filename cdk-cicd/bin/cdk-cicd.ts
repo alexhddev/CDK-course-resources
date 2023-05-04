198aeb24fcb68015b122d7667e36297986f21ff8
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-05-04 08:38:15 +0300

    Cicd project init

---
+++ cdk-cicd/bin/cdk-cicd.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkCicdStack } from '../lib/cdk-cicd-stack';
+
+const app = new cdk.App();
+new CdkCicdStack(app, 'CdkCicdStack', {
+});
---
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class CdkCicdStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+  }
+}
