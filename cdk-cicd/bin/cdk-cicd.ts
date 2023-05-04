43bd0207119288784d6b7014e2591633e9a1ada2
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-05-04 09:13:52 +0300

    Pipeline init

--- cdk-cicd/bin/cdk-cicd.ts
+++ cdk-cicd/bin/cdk-cicd.ts
@@ -6,3 +6,5 @@ import { CdkCicdStack } from '../lib/cdk-cicd-stack';
 const app = new cdk.App();
 new CdkCicdStack(app, 'CdkCicdStack', {
 });
+
+app.synth();
--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('alexhddev/CDK-course-resources', 'cicd-practice'),
+        commands: [
+          'cd cdk-cicd',
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out'
+      })
+    })
 
   }
 }
