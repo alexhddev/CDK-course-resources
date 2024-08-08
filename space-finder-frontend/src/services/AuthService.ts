06330a768e996e49e019c6b80efced24ab5b8670
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-08-08 11:58:02 +0300

    AuthService

--- space-finder-frontend/src/services/AuthService.ts
+++ space-finder-frontend/src/services/AuthService.ts
@@ -1,16 +1,44 @@
+import { Amplify } from 'aws-amplify';
+import { SignInOutput, fetchAuthSession, signIn } from '@aws-amplify/auth';
+import { AuthStack } from '../../../space-finder/outputs.json'
 
+const awsRegion = 'eu-west-1';
 
+Amplify.configure({
+    Auth: {
+      Cognito: {
+        userPoolId: AuthStack.SpaceUserPoolId,
+        userPoolClientId: AuthStack.SpaceUserPoolClientId,
+        identityPoolId: AuthStack.SpaceIdentityPoolId
+      },
+    },
+  });
 
 export class AuthService {
 
+    private user: SignInOutput | undefined;
+    private userName: string = '';
+
 
     public async login(userName: string, password: string):Promise<Object | undefined> {
-        return {
-            user: 'abc'
+        try {
+            const signInOutput: SignInOutput = await signIn({
+                username: userName,
+                password: password,
+                options: {
+                    authFlowType: 'USER_PASSWORD_AUTH'
+                }
+            });
+            this.user = signInOutput;
+            this.userName = userName;
+            return this.user;
+        } catch (error) {
+            console.error(error);
+            return undefined
         }
     }
 
     public getUserName(){
-        return 'some user'
+        return this.userName
     }
 }
--- space-finder-frontend/vite.config.ts
+++ space-finder-frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
 })
