46a31946b81adb261a49539aab2ac87e29e67760
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-08-08 12:18:18 +0300

    create space form

--- space-finder-frontend/src/App.tsx
+++ space-finder-frontend/src/App.tsx
@@ -4,8 +4,11 @@ import NavBar from './components/NavBar';
 import { useState } from 'react';
 import LoginComponent from './components/LoginComponent';
 import { AuthService } from './services/AuthService';
+import { DataService } from './services/DataService';
+import CreateSpace from './components/spaces/CreateSpace';
 
 const authService = new AuthService();
+const dataService = new DataService();
 
 function App() {
   const [userName, setUserName] = useState<string | undefined>(undefined);
@@ -33,7 +36,7 @@ function App() {
         },
         {
           path: "/createSpace",
-          element: <div>Create space page</div>,
+          element: <CreateSpace dataService={dataService}/>,
         },
         {
           path: "/spaces",
---
+++ space-finder-frontend/src/services/DataService.ts
@@ -0,0 +1,11 @@
+export class DataService {
+
+
+    public async createSpace(name: string, location:string, photo?: File){
+        return '123'
+    }
+
+    public isAuthorized(){
+        return true;
+    }
+}
